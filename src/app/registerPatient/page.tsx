fae8a4e67cbb9f14309098f86bd3f17348f31501
Sikandar-chaudhry <sikandarmukhtar5@gmail.com>
2024-09-07 10:19:28 +0500

    Add patient Login

--- src/app/registerPatient/page.tsx
+++ src/app/registerPatient/page.tsx
@@ -1,77 +1,76 @@
 import {Input} from "@/components/ui/input";
 import {Label} from "@/components/ui/label"
 import Link from 'next/link'
-import {register} from "@/action/user";
-import {getSession} from "@/lib/getSession";
-import {redirect} from "next/navigation";
 
 export default async function Register(){
 
-    const session = await getSession();
-    const user = session?.user;
-    if (user) redirect('/')
 
     return (
-        <div className="mt-10 max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white border border-[#121212]  dark:bg-black">
-            <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
-                Welcome to MyShop
-            </h2>
-            <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
-                Please provide all the necessary information
-            </p>
+        <div className='flex items-center justify-center'>
+            <div
+                className="mt-52 max-w-md w-full mx-auto rounded-none md:rounded-2xl  p-4 md:p-8 shadow-input bg-white md:border md:border-[#121212]  dark:bg-black">
+                <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+                    Welcome to Fury PMS
+                </h2>
+                <h3 className='text-lg font-medium'>Patient Registration</h3>
+                <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+                    Please provide all the necessary information
+                </p>
 
-            <form className="my-8" >
-                <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
-                    <div className="flex flex-col">
-                        <Label htmlFor="firstname" className="mb-2">
-                            First Name
-                        </Label>
-                        <Input
-                            id="firstname"
-                            placeholder="Tyler"
-                            type="text"
-                            name="firstname"
-                        />
+                <form className="my-8">
+                    <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+                        <div className="flex flex-col">
+                            <Label htmlFor="firstname" className="mb-2">
+                                First Name
+                            </Label>
+                            <Input
+                                id="firstname"
+                                placeholder="Tyler"
+                                type="text"
+                                name="firstname"
+                            />
+                        </div>
+                        <div className="flex flex-col">
+                            <Label htmlFor="lastname" className="mb-2">
+                                Last Name
+                            </Label>
+                            <Input
+                                id="lastname"
+                                placeholder="Durden"
+                                type="text"
+                                name="lastname"
+                            />
+                        </div>
                     </div>
-                    <div className="flex flex-col">
-                        <Label htmlFor="lastname" className="mb-2">
-                            Last Name
-                        </Label>
-                        <Input
-                            id="lastname"
-                            placeholder="Durden"
-                            type="text"
-                            name="lastname"
-                        />
-                    </div>
-                </div>
 
-                <Label htmlFor="email" className=''>Email Address</Label>
-                <Input
-                    id="email"
-                    placeholder="projectmayhem@fc.com"
-                    type="email"
-                    name="email"
-                    className='mt-2'
-                />
+                    <Label htmlFor="email" className=''>Email Address</Label>
+                    <Input
+                        id="email"
+                        placeholder="projectmayhem@fc.com"
+                        type="email"
+                        name="email"
+                        className='mt-2'
+                    />
 
-                <Label htmlFor="password">Password</Label>
-                <Input
-                    id="password"
-                    placeholder="***********"
-                    type="password"
-                    name="password"
-                    className="mb-5 mt-2"
-                />
+                    <Label htmlFor="password">Password</Label>
+                    <Input
+                        id="password"
+                        placeholder="***********"
+                        type="password"
+                        name="password"
+                        className="mb-5 mt-2"
+                    />
 
-                <button className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]">
-                    Sign up &rarr;
-                </button>
+                    <button
+                        className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]">
+                        Sign up &rarr;
+                    </button>
 
-            </form>
-            <p className="text-neutral-600 text-sm max-w-sm dark:text-neutral-300">
-                Already have an account? <Link href="/loginPatient" className='hover:underline'>Login</Link>
-            </p>
+                </form>
+                <p className="text-neutral-600 text-sm max-w-sm dark:text-neutral-300">
+                    Already have an account? <Link href="/patientLogin" className='hover:underline'>Login</Link>
+                </p>
+            </div>
         </div>
     );
 }
